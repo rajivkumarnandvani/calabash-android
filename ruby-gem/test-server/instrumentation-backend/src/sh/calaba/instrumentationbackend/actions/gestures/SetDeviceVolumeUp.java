3dc9825f456473cd3519def212f21041a0c730c8
rajivkumarnandvani <rajivkumarnandvani@gmail.com>
2013-12-24 14:46:18 +0530

    Create SetDeviceVolumeUp.java

---
+++ ruby-gem/test-server/instrumentation-backend/src/sh/calaba/instrumentationbackend/actions/gestures/SetDeviceVolumeUp.java
@@ -0,0 +1,23 @@
+package sh.calaba.instrumentationbackend.actions.gestures;
+
+import com.jayway.android.robotium.solo.Solo;
+
+import android.view.Display;
+import sh.calaba.instrumentationbackend.InstrumentationBackend;
+import sh.calaba.instrumentationbackend.Result;
+import sh.calaba.instrumentationbackend.actions.Action;
+
+public class SetDeviceVolumeUp implements Action {
+
+@Override
+public Result execute(String... args) {
+
+InstrumentationBackend.solo.setActivityOrientation(solo.sendKey(KeyEvent.KEYCODE_VOLUME_UP));
+return Result.successResult();
+}
+
+@Override
+public String key() {
+return "set_volumeup";
+}
+}
